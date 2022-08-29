8dd1cf70e0802f4fdda63785c5b84ad6d5590d8a
bverb098 <102575708+bverb098@users.noreply.github.com>
2022-08-29 23:15:58 +1000

    homework done without bonus

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -31,8 +31,15 @@ function createChart(svg) {
    return chartGroup
 };
 
+// create array of objects with data to pass into scatter plot
+function combineData(xData, yData, abbr) {
+   const combinedData = xData.map((x, i) => ({ x, y: yData[i], abbr: abbr[i]}));
+   console.log("combined data: ", combinedData)
+   return combinedData
+};
+
 // set axes scale with numerical data to both axes
-function numNumAxesScale (yData, xData) {
+function numNumAxesScale (xData, yData) {
    var yScale = d3.scaleLinear()
       .domain([d3.min(yData) * 0.8, d3.max(yData) * 1.1])
       .range([chartHeight, 0]);
@@ -72,49 +79,70 @@ function insertAxes(axes, chartGroup) {
       .call(axes.yAxis);
 };
 
-// append circles to chart
-function scatterCircles(data, xData, yData, chartGroup, axesScale) {
-   var scatterPoints = chartGroup.selectAll("g")
-      .data(data)
-   var g = scatterPoints
+// append circles with labels to chart in the correct position
+function scatterCircles(combinedData, chartGroup, axesScale) {
+   //create a group for every point that will be plotted. A circle and text will be appended under the group
+   var scatterPoints = chartGroup.selectAll("g points")
+      .data(combinedData)
       .enter()
       .append("g")
-      .attr("transform", d => `translate(${axesScale.xScale(d.income)},${axesScale.yScale(d.obesity)})`)
 
-   g.append("circle")
-      //.attr("cy", d => axesScale.yScale(d.obesity))
-      //.attr("cx", d => axesScale.xScale(d.income))
-      .attr("r", "10"):
-      .attr("fill", "red")
-      .
-   return g
+   // place a circle under each new group in the correct location
+   var radius = 8
+   var scatterCircles =scatterPoints.append("circle") 
+      .attr("r", `${radius}`)
+      .attr("cx", d => axesScale.xScale(d.x))
+      .attr("cy", d => axesScale.yScale(d.y))
+      .classed("stateCircle", true)
+
+   // place text at same spot with some adjustment to make sure they are middle of circles
+   var scatterLabels =scatterPoints.append("text")
+      .text(d => `${d.abbr}`)
+      .attr("x", d => axesScale.xScale(d.x))
+      .attr("y", d => axesScale.yScale(d.y) + (radius/4))
+      .attr("font-size", `${radius-2}`)
+      .classed("stateText", true);
+     
 };
 
-// append circle labels to chart
-function circleLabels(g, data, axesScale) {
-   g.append("text")
-      //.attr("cx", d => axesScale.xScale(d.income))
-      //.attr("cy", d => axesScale.yScale(d.obesity))
-      .text(d => d.abbr)
-};
+function axesLabels(chartGroup) {
+   var labelsGroup = chartGroup.append("g")
+      .attr("transform", `translate(${chartWidth/2},${chartHeight+20})`)
+   
+   // append x labels
+   var incomeLabel = labelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 20)
+      .classed("aText", true)
+      .text("income ($)")
 
+   // append y label
+   var obesityLabel = labelsGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", chartHeight/2) 
+      .attr("y", -chartWidth/2 - margin.left)
+      .attr("dy", "1em")
+      .classed("aText", true)
+      .text("rate of obesity (%)")
+};
 // -------------------- CREATE SCATTERPLOT --------------------
-function createScatterplot(healthData, obesity, income) {
+function createScatterplot(healthData, obesity, income, abbr) {
    // insert svg area to hold chart to page
    var svg = createSvg();
    // append chart group to svg area
    var chartGroup = createChart(svg);
+   // choose x values, y values and labels (state abbrevs)
+   var incomeObesity = combineData(income, obesity, abbr)
    // create scale functions
-   var axesScale = numNumAxesScale(obesity, income);
+   var axesScale = numNumAxesScale(income, obesity);
    // create axis functions
    var axes = scaleToAxes(axesScale);
    // append axes to chart
    insertAxes(axes, chartGroup);
    // append scatter plot circles to chart
-   // use income and obesity as x and y values
-   var g = scatterCircles(healthData, income , obesity, chartGroup, axesScale)
-   // add labels to scatterplot points
-   circleLabels(g,healthData,axesScale)
+   scatterCircles(incomeObesity, chartGroup, axesScale)
+   // append labels to axes
+   axesLabels(chartGroup)
 };
 // ------------------------------------------------------------
 
@@ -139,10 +167,10 @@ d3.csv(dataPath).then(healthData => {
    console.log("abbrv: ", stateAbbr);
    console.log("poverty: ", poverty);
    console.log("age: ", age);
-   console.log("income: ", healthData.income);
-   console.log("obesity: ", healthData.obesity);
+   console.log("income: ", income);
+   console.log("obesity: ", obesity);
    console.log("smokers: ", smokers);
    console.log("health data: ", healthData)
 
-   createScatterplot(healthData, obesity, income);
+   createScatterplot(healthData, obesity, income, stateAbbr);
 });
